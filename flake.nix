05bb9d1c1ea94a37bba697ca1b0d155fd573a347
arturo </run/secrets/git-email>
2025-12-24 14:55:23 -0500

    init commit

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    naersk.url = "github:nix-community/naersk";
+    git-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs = {flake-parts, ...} @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [inputs.git-hooks.flakeModule];
+
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+
+      perSystem = {system, ...}: let
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [inputs.rust-overlay.overlays.default];
+        };
+
+        # for additional versions: https://github.com/oxalica/rust-overlay
+        rustToolchain = pkgs.rust-bin.stable.latest.default;
+
+        naerskLib = pkgs.callPackage inputs.naersk {
+          cargo = rustToolchain;
+          rustc = rustToolchain;
+        };
+      in {
+        packages.default = naerskLib.buildPackage {src = ./.;};
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            rust-analyzer
+            openssl
+            pkg-config
+          ];
+
+          RUST_BACKTRACE = 1;
+
+          shellHook = ''
+            echo "🦀 $(rustc --version)"
+          '';
+        };
+
+        pre-commit = {
+          check.enable = true;
+          settings.hooks = {
+            rustfmt.enable = true;
+            clippy = {
+              enable = true;
+              settings.offline = false;
+            };
+          };
+        };
+      };
+    };
+}

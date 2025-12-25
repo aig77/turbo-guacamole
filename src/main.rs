eb9cd6b0940477dad4b421171c7bcc2b87134a15
arturo <29128253+aig77@users.noreply.github.com>
2025-12-25 18:29:14 -0500

    basic url shortening functionality

--- src/main.rs
+++ src/main.rs
@@ -1,149 +1,106 @@
-//! Simple in-memory key/value store showing features of axum.
-//!
-//! Run with:
-//!
-//! ```not_rust
-//! cargo run -p example-key-value-store
-//! ```
+use sha2::{Digest, Sha256};
+use std::collections::HashMap;
+use std::io;
 
-use axum::{
-    body::Bytes,
-    error_handling::HandleErrorLayer,
-    extract::{DefaultBodyLimit, Path, State},
-    handler::Handler,
-    http::StatusCode,
-    response::IntoResponse,
-    routing::{delete, get},
-    Router,
-};
-use std::{
-    borrow::Cow,
-    collections::HashMap,
-    sync::{Arc, RwLock},
-    time::Duration,
-};
-use tower::{BoxError, ServiceBuilder};
-use tower_http::{
-    compression::CompressionLayer, limit::RequestBodyLimitLayer, trace::TraceLayer,
-    validate_request::ValidateRequestHeaderLayer,
-};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+const BASE_URL: &str = "tg.com";
 
-#[tokio::main]
-async fn main() {
-    tracing_subscriber::registry()
-        .with(
-            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
-            }),
-        )
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+fn main() {
+    let mut db: HashMap<String, String> = HashMap::new();
+    loop {
+        println!("1) Add a URL");
+        println!("2) Get a URL");
+        println!("3) Print URLs");
+        println!("4) Exit");
+        println!("Select an option:");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        let choice: u8 = match input.trim().parse() {
+            Ok(num) => num,
+            _ => {
+                println!("Please enter a valid number\n");
+                continue;
+            }
+        };
 
-    let shared_state = SharedState::default();
+        match choice {
+            1 => {
+                println!("Input a URL:");
+                let mut url = String::new();
+                io::stdin()
+                    .read_line(&mut url)
+                    .expect("Failed to read line");
+                add_url(&mut db, url.trim());
+                println!("Shortened to {}/{}\n", BASE_URL, url.trim());
+            }
+            2 => {
+                if db.is_empty() {
+                    println!("Database is empty\n");
+                    continue;
+                }
 
-    // Build our application by composing routes
-    let app = Router::new()
-        .route(
-            "/{key}",
-            // Add compression to `kv_get`
-            get(kv_get.layer(CompressionLayer::new()))
-                // But don't compress `kv_set`
-                .post_service(
-                    kv_set
-                        .layer((
-                            DefaultBodyLimit::disable(),
-                            RequestBodyLimitLayer::new(1024 * 5_000 /* ~5mb */),
-                        ))
-                        .with_state(Arc::clone(&shared_state)),
-                ),
-        )
-        .route("/keys", get(list_keys))
-        // Nest our admin routes under `/admin`
-        .nest("/admin", admin_routes())
-        // Add middleware to all routes
-        .layer(
-            ServiceBuilder::new()
-                // Handle errors from middleware
-                .layer(HandleErrorLayer::new(handle_error))
-                .load_shed()
-                .concurrency_limit(1024)
-                .timeout(Duration::from_secs(10))
-                .layer(TraceLayer::new_for_http()),
-        )
-        .with_state(Arc::clone(&shared_state));
+                println!("Input a URL:");
+                let mut url = String::new();
+                io::stdin()
+                    .read_line(&mut url)
+                    .expect("Failed to read line");
+                println!("{}\n", get_url(&db, url.as_str().trim()));
+            }
+            3 => {
+                if db.is_empty() {
+                    println!("Database is empty\n");
+                    continue;
+                }
 
-    // Run our app with hyper
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
-    tracing::debug!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
-}
-
-type SharedState = Arc<RwLock<AppState>>;
-
-#[derive(Default)]
-struct AppState {
-    db: HashMap<String, Bytes>,
-}
-
-async fn kv_get(
-    Path(key): Path<String>,
-    State(state): State<SharedState>,
-) -> Result<Bytes, StatusCode> {
-    let db = &state.read().unwrap().db;
-
-    if let Some(value) = db.get(&key) {
-        Ok(value.clone())
-    } else {
-        Err(StatusCode::NOT_FOUND)
+                list_urls(&db);
+                println!();
+            }
+            4 => {
+                println!("Exiting");
+                break;
+            }
+            _ => {
+                println!("Invalid option\n");
+            }
+        }
     }
 }
 
-async fn kv_set(Path(key): Path<String>, State(state): State<SharedState>, bytes: Bytes) {
-    state.write().unwrap().db.insert(key, bytes);
+// DB operations
+fn add_url(db: &mut HashMap<String, String>, url: &str) {
+    db.insert(encode(url), url.to_string());
 }
 
-async fn list_keys(State(state): State<SharedState>) -> String {
-    let db = &state.read().unwrap().db;
-
-    db.keys()
-        .map(|key| key.to_string())
-        .collect::<Vec<String>>()
-        .join("\n")
+fn get_url(db: &HashMap<String, String>, url: &str) -> String {
+    // grab the last item in the url (encoded strings we're using as keys)
+    let key = url.split('/').next_back().unwrap().to_string();
+    match db.get(&key) {
+        Some(s) => s.to_string(),
+        _ => {
+            println!("Key not found!");
+            "".to_string()
+        }
+    }
 }
 
-fn admin_routes() -> Router<SharedState> {
-    async fn delete_all_keys(State(state): State<SharedState>) {
-        state.write().unwrap().db.clear();
+fn list_urls(db: &HashMap<String, String>) {
+    for (k, v) in db.iter() {
+        println!("{}/{}, {}", BASE_URL, k, v);
     }
-
-    async fn remove_key(Path(key): Path<String>, State(state): State<SharedState>) {
-        state.write().unwrap().db.remove(&key);
-    }
-
-    Router::new()
-        .route("/keys", delete(delete_all_keys))
-        .route("/key/{key}", delete(remove_key))
-        // Require bearer auth for all admin routes
-        .layer(ValidateRequestHeaderLayer::bearer("secret-token"))
 }
 
-async fn handle_error(error: BoxError) -> impl IntoResponse {
-    if error.is::<tower::timeout::error::Elapsed>() {
-        return (StatusCode::REQUEST_TIMEOUT, Cow::from("request timed out"));
-    }
+// fn shorten_url(url: &str) -> String {
+//     let mut s = String::from(BASE_URL);
+//     s.push('/');
+//     s.push_str(encode(url).as_str());
+//     s
+// }
 
-    if error.is::<tower::load_shed::error::Overloaded>() {
-        return (
-            StatusCode::SERVICE_UNAVAILABLE,
-            Cow::from("service is overloaded, try again later"),
-        );
-    }
-
-    (
-        StatusCode::INTERNAL_SERVER_ERROR,
-        Cow::from(format!("Unhandled internal error: {error}")),
-    )
+fn encode(s: &str) -> String {
+    let hash = Sha256::digest(s);
+    let bytes = hash.as_slice();
+    let eight_bytes: [u8; 8] = bytes[..8].try_into().unwrap();
+    let number = u64::from_be_bytes(eight_bytes);
+    base62::encode(number)
 }

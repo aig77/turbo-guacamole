2d061dcb17e20fff77d26a9e462e9696a6ced2df
arturo </run/secrets/git-email>
2025-12-25 20:02:25 -0500

    add simple axum template

--- src/main.rs
+++ src/main.rs
@@ -1,101 +1,96 @@
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    http::StatusCode,
+    routing::{delete, get, post},
+};
+use serde::Deserialize;
 use sha2::{Digest, Sha256};
-use std::collections::HashMap;
-use std::io;
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
 
 const BASE_URL: &str = "tg.com";
 
-fn main() {
-    let mut db: HashMap<String, String> = HashMap::new();
-    loop {
-        println!("1) Add a URL");
-        println!("2) Get a URL");
-        println!("3) Print URLs");
-        println!("4) Exit");
-        println!("Select an option:");
-        let mut input = String::new();
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
-        let choice: u8 = match input.trim().parse() {
-            Ok(num) => num,
-            _ => {
-                println!("Please enter a valid number\n");
-                continue;
-            }
-        };
+#[tokio::main]
+async fn main() {
+    let shared_state = SharedState::default();
 
-        match choice {
-            1 => {
-                println!("Input a URL:");
-                let mut url = String::new();
-                io::stdin()
-                    .read_line(&mut url)
-                    .expect("Failed to read line");
-                add_url(&mut db, url.trim());
-                println!("Shortened to {}/{}\n", BASE_URL, url.trim());
-            }
-            2 => {
-                if db.is_empty() {
-                    println!("Database is empty\n");
-                    continue;
-                }
+    let app = Router::new()
+        // will eventually route, for now just print the value
+        .route("/{key}", get(get_url))
+        .route("/shorten", post(add_url))
+        .nest("/admin", admin_routes())
+        .with_state(Arc::clone(&shared_state));
 
-                println!("Input a URL:");
-                let mut url = String::new();
-                io::stdin()
-                    .read_line(&mut url)
-                    .expect("Failed to read line");
-                println!("{}\n", get_url(&db, url.as_str().trim()));
-            }
-            3 => {
-                if db.is_empty() {
-                    println!("Database is empty\n");
-                    continue;
-                }
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    dbg!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
 
-                list_urls(&db);
-                println!();
-            }
-            4 => {
-                println!("Exiting");
-                break;
-            }
-            _ => {
-                println!("Invalid option\n");
-            }
-        }
+type SharedState = Arc<RwLock<AppState>>;
+
+#[derive(Default)]
+struct AppState {
+    db: HashMap<String, String>,
+}
+
+#[derive(Deserialize)]
+struct ShortenPayload {
+    url: String,
+}
+
+async fn get_url(
+    Path(key): Path<String>,
+    State(state): State<SharedState>,
+) -> Result<String, StatusCode> {
+    let db = &state.read().unwrap().db;
+
+    if let Some(value) = db.get(&key) {
+        let url = value.clone();
+        Ok(url)
+    } else {
+        Err(StatusCode::NOT_FOUND)
     }
 }
 
-// DB operations
-fn add_url(db: &mut HashMap<String, String>, url: &str) {
-    db.insert(encode(url), url.to_string());
+async fn add_url(
+    State(state): State<SharedState>,
+    Json(payload): Json<ShortenPayload>,
+) -> Result<String, StatusCode> {
+    let key = encode(payload.url.as_str());
+    let shortened = shortened_url_from_key(&key);
+    state.write().unwrap().db.insert(key, payload.url);
+    Ok(shortened)
 }
 
-fn get_url(db: &HashMap<String, String>, url: &str) -> String {
-    // grab the last item in the url (encoded strings we're using as keys)
-    let key = url.split('/').next_back().unwrap().to_string();
-    match db.get(&key) {
-        Some(s) => s.to_string(),
-        _ => {
-            println!("Key not found!");
-            "".to_string()
-        }
+fn admin_routes() -> Router<SharedState> {
+    async fn list_keys(State(state): State<SharedState>) -> Result<String, StatusCode> {
+        let db = &state.read().unwrap().db;
+
+        Ok(db
+            .keys()
+            .map(|key| key.to_string())
+            .collect::<Vec<String>>()
+            .join("\n"))
     }
-}
 
-fn list_urls(db: &HashMap<String, String>) {
-    for (k, v) in db.iter() {
-        println!("{}/{}, {}", BASE_URL, k, v);
+    async fn delete_all_keys(State(state): State<SharedState>) {
+        state.write().unwrap().db.clear();
     }
-}
 
-// fn shorten_url(url: &str) -> String {
-//     let mut s = String::from(BASE_URL);
-//     s.push('/');
-//     s.push_str(encode(url).as_str());
-//     s
-// }
+    async fn remove_key(Path(key): Path<String>, State(state): State<SharedState>) {
+        state.write().unwrap().db.remove(&key);
+    }
+
+    Router::new()
+        .route("/keys", get(list_keys))
+        .route("/delete/keys", delete(delete_all_keys))
+        .route("/delete/keys/{key}", delete(remove_key))
+}
 
 fn encode(s: &str) -> String {
     let hash = Sha256::digest(s);
@@ -103,4 +98,11 @@ fn encode(s: &str) -> String {
     let eight_bytes: [u8; 8] = bytes[..8].try_into().unwrap();
     let number = u64::from_be_bytes(eight_bytes);
     base62::encode(number)
+}
+
+fn shortened_url_from_key(key: &str) -> String {
+    let mut shortened = String::from(BASE_URL);
+    shortened.push('/');
+    shortened.push_str(key);
+    shortened
 }

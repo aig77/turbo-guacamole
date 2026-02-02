769006d68ebfb3a68531328d3e99566baa63e325
Arturo Galan <arturo@Arturos-MacBook-Pro.local>
2026-02-02 13:03:07 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -1,4 +1,15 @@
 {
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
@@ -43,6 +54,8 @@
             rust-analyzer
             pre-commit
             docker-compose
+            postgresql
+            redis
           ];
 
           RUST_BACKTRACE = 1;
